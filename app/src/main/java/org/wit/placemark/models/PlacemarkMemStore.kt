ac6c7d3d3bb386347a01c3e1324754a026db320f
EmmetKellyWIT <20075740@mail.wit.ie>
2019-09-05 19:14:19 -0700

    Initial commit

---
+++ app/src/main/java/org/wit/placemark/models/PlacemarkMemStore.kt
@@ -0,0 +1,44 @@
+package org.wit.placemark.models
+
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+var lastId = 0L
+
+internal fun getId(): Long {
+    return lastId++
+}
+
+class PlacemarkMemStore : PlacemarkStore, AnkoLogger {
+
+    val placemarks = ArrayList<PlacemarkModel>()
+
+    override fun findAll(): List<PlacemarkModel> {
+        return placemarks
+    }
+
+    override fun create(placemark: PlacemarkModel) {
+        placemark.id = getId()
+        placemarks.add(placemark)
+        logAll()
+    }
+
+    override fun update(placemark: PlacemarkModel) {
+        var foundPlacemark: PlacemarkModel? = placemarks.find { p -> p.id == placemark.id }
+        if (foundPlacemark != null) {
+            foundPlacemark.title = placemark.title
+            foundPlacemark.description = placemark.description
+            foundPlacemark.warnings = placemark.warnings
+            foundPlacemark.image = placemark.image
+            logAll()
+        }
+    }
+
+    override fun delete(placemark: PlacemarkModel) {
+        placemarks.remove(placemark)
+    }
+
+    fun logAll() {
+        placemarks.forEach { info("${it}") }
+    }
+}
